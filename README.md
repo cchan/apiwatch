25ef32012d0c3af1b8f77c5abb45961eed1242b6
Clive Chan <cc@clive.io>
2017-08-05 00:33:28 -0400

    added readme and sample urls

---
+++ README.md
@@ -0,0 +1,4 @@
+# apiwatch
+
+Watches an API URL for changes, ignoring differences in JSON ordering.
+
---
+++ urls.sample.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "SF weather": "https://someweatherapi.com/usa/ca/sf",
+    "Course selection results": "https://myuniversity.edu/api/courses/me?term=fall2017&token=63718748282874788272900384
+}
+
